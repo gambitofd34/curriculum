6233bc35865e611fa02d5a39394a7e1a80550e73
juancamilo30 <camicastilla200130@gmail.com>
2023-05-12 16:46:25 -0500

    update responsi

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,18 @@
-alert("Hello bienvenido");
+let enlaces_header = document.querySelectorAll(".enlaces-header")[0];
+let icon_hamburguer = true ;
+
+
+document.querySelectorAll(".hamburguer")[0].addEventListener("click",function(){
+    if(icon_hamburguer){
+        document.querySelectorAll(".hamburguer")[0].style.color="#fff";
+        icon_hamburguer=false;
+    }else{
+        document.querySelectorAll(".hamburguer")[0].style.color = "#000";
+        icon_hamburguer=true;
+    }
+    enlaces_header.classList.toggle("hamburgueropen");
+})
+document.querySelector(".enlaces_header")[0].addEventListener("click",function(){
+    enlaces_header.classList.toggle("hamburgueropen");
+    document.querySelectorAll(".hamburguer")[0].style.color = "#000";
+})
